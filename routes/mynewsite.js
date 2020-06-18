c6e21e7196c2525905a569296de5e8d17528d330
manwinder768 <manwinderkular768@gmail.com>
2020-06-17 23:15:10 -0700

    updated

--- routes/mynewsite.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('coolsite.ejs', { title: 'My New Website' });
-});
-
-module.exports = router;
--- routes/users.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('Hello from the GET call to the server');
-});
-
-module.exports = router;
